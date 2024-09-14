cd70b6c399faa88dfcb8d3884a5248581aae9982
tonthatvu101982 <tonthatvu101982@gmail.com>
2024-09-14 10:01:04 +0700

    initialize udemy ms es project

--- src/SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net8.0/Post.Query.Domain.AssemblyInfo.cs
+++ src/SM-Post/Post.Query/Post.Query.Domain/obj/Debug/net8.0/Post.Query.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab542f10e5c096ede330788d831954fae615f29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post.Query.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
